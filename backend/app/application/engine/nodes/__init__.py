d2f41bdc8e0a001f824d866eb70406ecd98c3c87
AndrewBondaren <Andr.bondaren@gmail.com>
2026-06-03 16:29:48 +0300

    Terrain Summary Node

--- backend/app/application/engine/nodes/__init__.py
+++ backend/app/application/engine/nodes/__init__.py
@@ -5,10 +5,11 @@ from app.application.engine.nodes.pojo.llm import IntentDetectionNode, ContextSn
 
 #Python Nodes.
 from app.application.engine.nodes.pojo.python import prepareAnalysisContextNode
-from app.application.engine.nodes.pojo.python import checkSceneNode
-from app.application.engine.nodes.pojo.python import sceneInitNode
-from app.application.engine.nodes.pojo.python import sceneLocationSelectNode
+from app.application.engine.nodes.pojo.python.scene import checkSceneNode
+from app.application.engine.nodes.pojo.python.scene import sceneInitNode
+from app.application.engine.nodes.pojo.python.scene import sceneLocationSelectNode
 from app.application.engine.nodes.pojo.python.terrain import checkTerrainNode
 from app.application.engine.nodes.pojo.python.terrain import eagerTerrainNode
 from app.application.engine.nodes.pojo.python.terrain import lazyTerrainNode
 from app.application.engine.nodes.pojo.python.terrain import terrainContextNode
+from app.application.engine.nodes.pojo.python.terrain import terrainSummaryNode
